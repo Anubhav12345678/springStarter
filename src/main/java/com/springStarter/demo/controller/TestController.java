313ee02e8c774586b24fbd945c75cc8c903457f6
anubhavgupta408 <anubhavgupta408@gmail.com>
2021-12-30 17:59:49 +0530

    Update and Delete functionality also added

--- src/main/java/com/springStarter/demo/controller/TestController.java
+++ src/main/java/com/springStarter/demo/controller/TestController.java
@@ -7,11 +7,14 @@ import com.springStarter.demo.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
@@ -35,5 +38,26 @@ public class TestController {
     public User addUser(@RequestParam("name") String name,@RequestParam("email") String email) {
        return userRepository.save(new User(name,email));
     }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable("id") int id) {
+       return userRepository.findById(id).get();
+    }
+
+    @PostMapping(value="/users/{id}")
+    public User updateUser(@PathVariable("id") int id,@RequestParam("name") String name, @RequestParam("email") String email) {
+        User user = userRepository.findById(id).get();
+        user.setEmail(email);
+        user.setName(name);
+        User response = userRepository.save(user);
+        return response;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+       userRepository.deleteById(id);
+       return "User Deleted Successfully";
+    }
+    
 
 }
